e53321a48643cc1c33bd008c006b6fef83cbb620
Groot <ericfinney1@gmail.com>
2019-03-31 12:31:17 -0400

    3/31 updates

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Task from "./pages/Task";
+import Tasks from "./pages/Tasks";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route path="/Task" component={Task} />
+          <Route path="/Tasks" component={Tasks} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
