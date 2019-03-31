5af73c5677756d8014f2949b6d55908784a14345
Groot <ericfinney1@gmail.com>
2019-03-31 13:47:56 -0400

    added routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
 import React, { Component } from "react";
-// import { BrowserRouter as Router, Route } from "react-router-dom";
-// import Home from "./pages/Home";
-// import Login from "./pages/Login";
-// import Register from "./pages/Register";
-// import Task from "./pages/Task";
-// import Tasks from "./pages/Tasks";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Task from "./pages/Task";
+import Tasks from "./pages/Tasks";
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello. Track-a-task underconstruction. </h1>
-      // <Router>
-      //   <div>
-      //     <Route exact path="/" component={Home} />
-      //     <Route exact path="/login" component={Login} />
-      //     <Route exact path="/register" component={Register} />
-      //     <Route path="/Task" component={Task} />
-      //     <Route path="/Tasks" component={Tasks} />
-      //   </div>
-      // </Router>
+      <div>
+        <h1>Hello. Track-a-task underconstruction. </h1>
+        <Router>
+          <div>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+            <Route path="/Task" component={Task} />
+            <Route path="/Tasks" component={Tasks} />
+          </div>
+        </Router>
+      </div>
     );
   }
 }
