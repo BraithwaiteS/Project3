2da06693d3a2e75d3c2a820d26dc43c7310d49e1
AMcDuffie <mcduarle@aol.com>
2019-04-06 12:51:28 -0400

    routeupdate

--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -1,17 +1,77 @@
-const router = require("express").Router();
-const booksController = require("../../controllers/tasksController");
+// Requiring our Todo model
+var db = require("../models");
 
-// Matches with "/api/tasks"
-router
-  .route("/")
-  .get(booksController.findAll) //change this to the tasksController findall sequelize equivilent
-  .post(booksController.create); //change this to the tasksController create sequelize equivilent
+// Routes
+// =============================================================
+module.exports = function(app) {
 
-// Matches with "/api/tasks/:id"
-router
-  .route("/:id")
-  .get(booksController.findById) //change this to the tasksController findbyid sequelize equivilent
-  .put(booksController.update) //change this to the tasksController update sequelize equivilent
-  .delete(booksController.remove); //change this to the tasksController remove sequelize equivilent
+  // GET route for getting all of the tasks
+  app.get("/api/task/", function(req, res) {
+    db.Task.findAll({})
+      .then(function(dbTask) {
+        res.json(dbTask);
+      });
+  });
 
-module.exports = router;
+  // Get route for returning posts of a specific task
+  app.get("/api/task/taskName/:taskName", function(req, res) {
+    db.Task.findAll({
+      where: {
+       taskName: req.params.taskName
+      }
+    })
+      .then(function(dbTask) {
+        res.json(dbTask);
+      });
+  });
+
+  // Get route for retrieving a single post
+  app.get("/api/task/:id", function(req, res) {
+    db.Task.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(function(dbTask) {
+        res.json(dbTask);
+      });
+  });
+
+  // POST route for saving a new task
+  app.post("/api/task", function(req, res) {
+    console.log(req.body);
+    db.Task.create({
+      taskName: req.body.Task,
+      due_date: req.body.DueDate,
+      completed: req.body.Completion
+    })
+      .then(function(dbTask) {
+        res.json(dbTask);
+      });
+  });
+
+  // DELETE route for deleting posts
+  app.delete("/api/task/:id", function(req, res) {
+    db.Task.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(function(dbTask) {
+        res.json(dbTask);
+      });
+  });
+
+  // PUT route for updating task
+  app.put("/api/task", function(req, res) {
+    db.Task.update(req.body,
+      {
+        where: {
+          id: req.body.id
+        }
+      })
+      .then(function(dbTask) {
+        res.json(dbTask);
+      });
+  });
+};
