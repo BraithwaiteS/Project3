2f4b850d29e7314b5116ff668e52f221d161698e
Arlette McDuffie <38673010+AMcDuffie@users.noreply.github.com>
2019-04-09 08:58:12 -0400

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,20 +1,16 @@
-// need to creat a new users// Requiring our Todo model
-var db = require("../../models");
-
 // Routes
 // =============================================================
-module.exports = function(app) {
+var db = require("../../models");
+const router = require("express").Router();
 
  // Get route for returning a specific user
- app.get("/api/users", function(req, res) {
+ router.get("/users", function(req, res) {
   db.User.findAll({
     where: {
      UserId: req.query.id
     }
   })
     .then(function(dbUser) {
-      console.log("get users");
       res.json(dbUser);
     });
 })
-}
